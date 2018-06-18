41fdd2cdba15c84022233833ce75c5464cf4c3f8
Thomas Castleman <thomas7rex@gmail.com>
2018-06-17 20:33:12 -0400

    Basic basic app

---
+++ server.js
@@ -0,0 +1,17 @@
+
+var express 			= require('express');
+var app 				= express();
+// var mustacheExpress 	= require('mustache-express');
+// var bodyParser 			= require('body-parser');
+// var cookieParser 		= require('cookie-parser');
+// var session 			= require('cookie-session');
+// var moment 				= require('moment');
+// var GoogleStrategy 		= require('passport-google-oauth2').Strategy;
+// var passport 			= require('passport');
+
+
+
+
+var server = app.listen(8080, function() {
+	console.log("StabOverflow server listening on port %d", server.address().port);
+});
