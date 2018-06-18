bf87bd36a1e0c1254f295a86e53586feda25ffe9
Thomas Castleman <thomas7rex@gmail.com>
2018-06-17 23:08:38 -0400

    Adding oauth / passport

--- server.js
+++ server.js
@@ -8,6 +8,62 @@ var moment 				= require('moment');
 var session 			= require('cookie-session');
 var GoogleStrategy 		= require('passport-google-oauth2').Strategy;
 var passport 			= require('passport');
+var creds				= require('./credentials.js');
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine('html', mustacheExpress());
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/views'));
+
+passport.serializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+	done(null, user);
+});
+
+passport.use(new GoogleStrategy({
+		clientID:		creds.GOOGLE_CLIENT_ID,
+		clientSecret:	creds.GOOGLE_CLIENT_SECRET,
+		callbackURL:	creds.domain + "/auth/google/callback",
+		passReqToCallback: true
+	},
+	function(request, accessToken, refreshToken, profile, done) {
+		process.nextTick(function () {
+			return done(null, profile);
+		});
+	}
+));
+
+app.use(session({ 
+	secret: creds.SESSION_SECRET,
+	name: 'session',
+	resave: true,
+	saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/auth/google', passport.authenticate('google', { scope: [
+	'https://www.googleapis.com/auth/userinfo.profile',
+	'https://www.googleapis.com/auth/userinfo.email'] 
+}));
+
+app.get('/auth/google/callback',
+	passport.authenticate('google', { 
+		successRedirect: '/',
+		failureRedirect: '/failure'
+}));
+
+app.get('/logout', function(req, res){
+	req.logout();
+	res.redirect('/');
+});
 
 var server = app.listen(8080, function() {
 	console.log("StabOverflow server listening on port %d", server.address().port);
