526da141e12e2ba0255feffddc7626e26365a2a8
Thomas Castleman <thomas7rex@gmail.com>
2018-08-21 19:26:20 -0400

    Update footer / settings

--- settings.js
+++ settings.js
@@ -7,19 +7,19 @@ module.exports = {
 
 	// search settings
 	search: {
-		resultsPerPage: 2,	// number of search results per each page
+		resultsPerPage: 25,	// number of search results per each page
 		maxNumResults: 300	// maximum number of total results yielded by a search request (this should be > resultsPerPage)
 	},
 
 	// user search settings
 	userSearch: {
-		usersPerPage: 4,	// number of user search results per page
+		usersPerPage: 25,	// number of user search results per page
 		maxNumResults: 300	// maximum number of total results yielded for a user search
 	},
 
 	// visitor-related settings
 	visitor: {
 		numQuestionsOnLanding: 50,		// number of recent questions shown on the landing page
-		numPostsOnUserPage: 20			// number of posts shown in the "Recent Posts" section of each user page
+		numPostsOnUserPage: 15			// number of posts shown in the "Recent Posts" section of each user page
 	}
 }
